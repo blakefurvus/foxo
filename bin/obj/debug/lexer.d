8689d7016f1451b42a8a6573995e5345c47c3e47
blakefurvus <blakefurvus@gmail.com>
2020-01-25 01:17:09 +0300

    Cleanup

--- bin/obj/debug/lexer.d
+++
@@ -1,8 +0,0 @@
-../bin/obj/debug/lexer.o: src/lexer.c include/lexer.h include/tok.h \
-  include/var_types.h
-
-include/lexer.h:
-
-include/tok.h:
-
-include/var_types.h:
--- bin/obj/debug/tok.d
+++
@@ -1,5 +0,0 @@
-../bin/obj/debug/tok.o: src/tok.c include/tok.h include/var_types.h
-
-include/tok.h:
-
-include/var_types.h:
